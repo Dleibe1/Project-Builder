228b5e9415b58a098e5ab2b26b2731662da26e64
dleibe1 <daniel.leibensperger28@gmail.com>
2024-06-21 12:32:00 -0400

    Initial setup working

--- client/src/components/layout/ForkProjectButton.js
+++ client/src/components/layout/ForkProjectButton.js
@@ -1,10 +1,13 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import Button from '@mui/material/Button';
+
 
 const ForkProjectButton = ({ id }) => {
   return (
     <Link to={`/fork-projects/${id}`}>
-      <button id="edit-build-button">Fork Project</button>
+      <Button variant="contained">Hello world</Button>;
+      {/* <button id="edit-build-button">Fork Project</button> */}
     </Link>
   )
 }
