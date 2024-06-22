d8f73f8ef87efe764988a539fa5b0ef514b38763
dleibe1 <daniel.leibensperger28@gmail.com>
2024-06-22 13:42:56 -0400

    removed unneccesary topbar parts

--- client/src/components/layout/TopBar.js
+++ client/src/components/layout/TopBar.js
@@ -62,41 +62,6 @@ const TopBar = ({ user }) => {
     location.href = "/project-list"
   }
 
-  const unauthenticatedListItems = [
-    <li key="sign-in">
-      <Link className="sign-in button authentication-button" to="/user-sessions/new">
-        Sign In
-      </Link>
-    </li>,
-    <li key="sign-up" className="sign-up">
-      <Link to="/users/new" className="button authentication-button">
-        Sign Up
-      </Link>
-    </li>,
-    <li>
-      <GithubLogin />
-    </li>,
-  ]
-
-  const authenticatedListItems = [
-    <li key="sign-out">
-      <SignOutButton id="sign-out" />
-    </li>,
-    <li>
-      <Link id="create-build" to="/create-new-build">
-        <Button variant="contained">Create Build</Button>
-      </Link>
-    </li>,
-    <li>
-      <Link id="my-builds" key={"my-builds"} to="/my-builds">
-        <Button variant="contained">My Builds</Button>
-      </Link>
-    </li>,
-    <li>
-      <UsernameTile user={user} />
-    </li>,
-  ]
-
   return (
     <AppBar position="static">
       <Container maxWidth="xl">
