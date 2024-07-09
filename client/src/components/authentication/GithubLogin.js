427bc9313e423c1bac7c52f2054ab931cea3ebd0
dleibe1 <daniel.leibensperger28@gmail.com>
2024-07-08 21:46:56 -0400

    fixed broken github logo

--- client/src/components/authentication/GithubLogin.js
+++ client/src/components/authentication/GithubLogin.js
@@ -28,7 +28,7 @@ const GithubLogin = (props) => {
         },
       }}
     >
-      <img onClick={handleGithubLogin} id="github-logo" src="https://i.imgur.com/9aand0d.png" />
+      <img onClick={handleGithubLogin} id="github-logo" src="images/github-logo.png" />
       LOGIN WITH GITHUB
     </Button>
   )
