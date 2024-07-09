a94f8eaa72cf3054cfd970ef0dd8275c5c341a5c
dleibe1 <daniel.leibensperger28@gmail.com>
2024-07-09 13:10:12 -0400

    added more seed data

---
+++ server/src/db/ProjectSeederData/descriptions.js
@@ -0,0 +1,18 @@
+const descriptions = [
+  `The two eyes are painted on two ping-pong balls which are sticked on two servo motors.  Two ultrasonic sensors measure the object's position. An Arduino Nano Every board reads the measurements from the ultrasonic sensors and controls the sevo motors accordingly.  The servo motors are SG90 micro servos and the ultrasonic sensors are HC-SR04. These are very common and very inexpensive devices. The Arduino board may be of another model than the nano Every, provided that 5V is available to power the servo motors and the ultrasonic sensors. `,
+  
+  `Gather your tools and troops (components, that is):
+  The brains: An Arduino Uno, the mastermind behind the magic. 
+  The translator: An HC05 Bluetooth module, your car's interpreter for smartphone commands. 
+  The muscle: L298N motor driver, the beefy guy controlling those zippy wheels. 
+  The bling: NeoPixel LEDs, because who doesn't love a car with dazzling lights? 
+  The base: Perfboard and Sunboard, the sturdy foundation for your creation. 
+  The connections: Jumper wires, the colorful threads that bring everything together. 
+  The fuel: A battery, to keep your robot rolling (safely, of course!). `,
+  
+  `In the world of do-it-yourself (DIY) projects, few activities match the practicality of constructing your own Arduino-powered automatic watering system. Imagine a setup where technology integrates effortlessly with nature, allowing you to take control of your plant care routine. We'll break down the fundamental components of this project, highlighting the vital role of the DS1302 timekeeping module and Arduino-compatible pumps. Get ready for a hands-on approach that not only enhances your technical skills but also transforms the way you care for your plants, whether they are in the corners of your home or under the open sky. `,
+  
+  `The spectrum analyzer displays the amplitude of signals as a function of frequency, allowing engineers and technicians to visualize and analyze signal characteristics. In particular, the audio analyzer performs a visual presentation of an acoustic signal in the frequency domain, where the frequency of the signal is displayed on the x-axis, while the amplitude of a certain frequency is displayed on the y-axis. In several of my previous $ videos $ I have presented several different types of such devices, but this time the FHT Arduino library is used for the first time. This library is several times faster than the commonly used FFT library, but at the expense of speed certain loss of resolution and precision appear at the two ends of the audio range. `,
+  ]
+
+  export default descriptions
