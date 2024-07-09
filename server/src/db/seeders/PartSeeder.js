a30dabaa3dc209fe62ce3f64ed84008ce648b466
dleibe1 <daniel.leibensperger28@gmail.com>
2024-07-09 16:35:38 -0400

    seeded parts and images for project forks

--- server/src/db/seeders/PartSeeder.js
+++ server/src/db/seeders/PartSeeder.js
@@ -25,6 +25,31 @@ class PartSeeder {
       { partName: "TIP120 Transistor", projectId: 9 },
       { partName: "LIS3DH Triple-Axis Accelerometer", projectId: 9 },
 
+    ])
+
+    //Project Fork Parts
+     await Part.query().insert([
+      { partName: "Arduino", projectId: 10 },
+      { partName: "Breadboard", projectId: 10 },
+      { partName: "Arduino", projectId: 11 },
+      { partName: "H-bridge", projectId: 11 },
+      { partName: "ESP32", projectId: 12 },
+      { partName: "H-bridge", projectId: 12 },
+      { partName: "Arduino", projectId: 13 },
+      { partName: "RBG LED", projectId: 13 },
+      { partName: "Arduino Uno Rev3", projectId: 14 },
+      { partName: "SSD1306 OLED Display", projectId: 14 },
+      { partName: "Breadboard - 400 contacts", projectId: 14 },
+      { partName: "USB 2.0 Cable Type A/B", projectId: 14 },
+      { partName: "Ultrasonic Sensor Module", projectId: 15 },
+      { partName: "IR Sensor Module - LM393", projectId: 15 },
+      { partName: "Tower Pro SG90 Servo Motor", projectId: 15 },
+      { partName: "Arduino Micro", projectId: 16 },
+      { partName: "W5500 mini Ethernet module", projectId: 16 },
+      { partName: "Arduino UNO SMD", projectId: 17 },
+      { partName: "Arduino UNO R4 WIFI", projectId: 18 },
+      { partName: "TIP120 Transistor", projectId: 18 },
+      { partName: "LIS3DH Triple-Axis Accelerometer", projectId: 18 },
 
     ])
   }
