1d9729ed376f2e5a8b6707064aa991afbade4d69
dleibe1 <daniel.leibensperger28@gmail.com>
2024-07-31 14:18:49 -0400

    Landing page nearly complete

--- client/src/components/layout/LandingPage.js
+++ client/src/components/layout/LandingPage.js
@@ -13,11 +13,19 @@ const LandingPage = (props) => {
     <>
       <div className="banner landing-page-section-container-left top-banner">
         <img className="banner-image" src="https://i.imgur.com/dOywizt.png" />
-        <h1>Create and Fork Micro Controller Projects.</h1>
+        <h1>Create and Fork Micro Controller Projects</h1>
       </div>
       <div className="banner second-banner landing-page-section-container-right">
         <img className="banner-image" src="https://i.imgur.com/4e9OsdZ.png" />
         <h1>Forking a project is the sincerest form of flattery.</h1>
+      </div>
+      <div className="banner third-banner landing-page-section-container-left">
+        <img className="banner-image" src="https://i.imgur.com/n8C2mDO.png" />
+        <h1>Sometimes one project's code can be used with a different set of parts.</h1>
+      </div>
+      <div className="banner fourth-banner landing-page-section-container-right">
+        <img className="banner-image" src="https://i.imgur.com/zNVBrY3.png" />
+        <h1>With Project Builder, you can fork a project's parts, code or both.</h1>
       </div>
     </>
   )
