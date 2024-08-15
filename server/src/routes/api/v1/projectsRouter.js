a43c2118954306cd8d09eff27f7e30538914d1da
dleibe1 <daniel.leibensperger28@gmail.com>
2024-08-15 14:45:46 -0400

    fixed issue with updating projects

--- server/src/routes/api/v1/projectsRouter.js
+++ server/src/routes/api/v1/projectsRouter.js
@@ -37,8 +37,6 @@ projectsRouter.get("/:id", async (req, res) => {
 
 projectsRouter.delete("/:id", async (req, res) => {
   const projectId = req.params.id
-  console.log(projectId)
-  console.log(req.user)
   try {
     await Part.query().delete().where("projectId", projectId)
     await Instruction.query().delete().where("projectId", projectId)
