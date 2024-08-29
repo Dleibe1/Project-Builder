b21d6bea936bc86d037957063481c178d7adec7f
dleibe1 <daniel.leibensperger28@gmail.com>
2024-08-29 15:43:35 -0400

    fixed styling issue with see forked versions button

--- client/src/components/layout/ProjectForksButton.js
+++ client/src/components/layout/ProjectForksButton.js
@@ -1,26 +1,27 @@
 import React from "react"
 import { Link } from "react-router-dom"
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button"
 
 const ProjectForksButton = ({ id }) => {
   return (
-      <Button
-        component={Link}
-        to={`/project-forks/${id}`}
-        key={`project-tile-forks-button${id}`}
-        sx={{
-          my: 2,
+    <Button
+      component={Link}
+      to={`/project-forks/${id}`}
+      key={`project-tile-forks-button${id}`}
+      sx={{
+        my: 2,
+        color: "white",
+        margin: "0px",
+        backgroundColor: "#1976d2",
+        "&:hover": {
+          backgroundColor: "#1665c0",
           color: "white",
-          margin:"0px",
-          backgroundColor: "#1976d2", 
-          "&:hover": {
-            backgroundColor: "#1665c0",
-            color: "white",
-          },
-        }}
-      >
-        See Forked Versions
-      </Button>
+        },
+      }}
+      className="see-forked-versions-button"
+    >
+      See Forked Versions
+    </Button>
   )
 }
 
