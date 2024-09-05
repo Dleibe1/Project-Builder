a49956350b12bd0dbf40147723a89ddcca9a1af5
dleibe1 <daniel.leibensperger28@gmail.com>
2024-09-05 09:16:25 -0400

    creating new branch

--- server/src/services/handleForkProject.js
+++ server/src/services/handleForkProject.js
@@ -18,28 +18,28 @@ const handleForkProject = async (originalProjectId, userId, forkData) => {
     code: forkData.code,
     parentProjectId: originalProjectId,
   })
-  
+
   const parts = forkData.parts
   const instructions = forkData.instructions
   const forkedProjectId = parseInt(forkedProject.id)
-  await Promise.all([
-    ...parts.map((part) => {
-      return Part.query().insert({ projectId: forkedProjectId, partName: part.partName })
-    }),
-    ...instructions.map((instruction) => {
-      if (instruction.imageURL) {
-        return Instruction.query().insert({
-          projectId: forkedProjectId,
-          imageURL: instruction.imageURL,
-        })
-      } else if (instruction.instructionText) {
-        return Instruction.query().insert({
-          projectId: forkedProjectId,
-          instructionText: instruction.instructionText,
-        })
-      }
-    }),
-  ])
+  const partInsertions = parts.map(async (part) => {
+    return await Part.query().insert({ projectId: forkedProjectId, partName: part.partName })
+  })
+
+  const instructionInsertions = instructions.map(async (instruction) => {
+    if (instruction.imageURL) {
+      return await Instruction.query().insert({
+        projectId: forkedProjectId,
+        imageURL: instruction.imageURL,
+      })
+    } else if (instruction.instructionText) {
+      return await Instruction.query().insert({
+        projectId: forkedProjectId,
+        instructionText: instruction.instructionText,
+      })
+    }
+  })
+  await Promise.all([...partInsertions, ...instructionInsertions])
 }
 
 export default handleForkProject
