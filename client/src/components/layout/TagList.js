7fae8663df74de64c19cd6c0aede08eddc00685f
dleibe1 <daniel.leibensperger28@gmail.com>
2025-02-10 08:58:25 -0500

    tags working in forms and show pages

--- client/src/components/layout/TagList.js
+++ client/src/components/layout/TagList.js
@@ -1,14 +1,25 @@
-import React from "react"
+import React, { useContext } from "react"
+import { useHistory } from "react-router-dom"
+import { TagContext } from "../../contexts/TagContext"
 import Chip from "@mui/material/Chip"
 import Stack from "@mui/material/Stack"
 
 const TagList = ({ tags }) => {
-  const handleClick = () => {
-
+  const history = useHistory()
+  const { selectedTag, setSelectedTag } = useContext(TagContext)
+  const tagSearch = (tagName) => {
+    history.push(`/?tag=${tagName}`)
+    setSelectedTag(tagName)
   }
-
   const tagChips = tags.map((tag) => {
-    return <Chip className="tag-chip" size="large" label={tag.tagName} onClick={handleClick} />
+    return (
+      <Chip
+        className="tag-chip"
+        size="large"
+        label={tag.tagName}
+        onClick={() => tagSearch(tag.tagName)}
+      />
+    )
   })
 
   return (
