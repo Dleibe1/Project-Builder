75ac704cf33661891643036c915c6d89cfbc598e
dleibe1 <daniel.leibensperger28@gmail.com>
2025-04-08 14:23:22 -0400

    userEditsProjectSubmitForm completed

--- e2e/cypress/integration/16_userEditsProjectSubmitForm.js
+++ e2e/cypress/integration/16_userEditsProjectSubmitForm.js
@@ -12,32 +12,26 @@ const getIframeBody = () => {
 describe("When I submit a project", () => {
   beforeEach(() => {
     truncateAllTables()
-      .then(() => {
         seedOneProject("exampleUser")
-      })
-      .then(() => {
         cy.loginUser("exampleUser")
-      })
-      .then(() => {
-        cy.visit("/edit-my-build/1")
-      })
   })
   it("I am using Chrome browser because Electron 87 browser cannot interact with TinyMCE form properly", () => {
     expect(Cypress.isBrowser("chrome")).to.be.true
   })
-  it("When I submit the form I am re-directed to my-builds-list", () => {
+  it("When I submit the form I am re-directed to my-builds-list and the project is visible there", () => {
+    cy.visit("/edit-my-build/1")
     cy.getByData("edit-project-title-input").should("have.value", "Interfacing RGB Led with Arduino")
     cy.getByData("edit-project-form").submit()
-    cy.url().should("include", "/my-builds-list")
-  })
-  it("The project I edited should be in my builds list and I can navigate to the project's show page", () => {
-    cy.visit("/my-builds-list")
+    cy.location("pathname", { timeout: 10000 }).should("include", "/my-builds-list")
     cy.getByData("my-build-tile", { timeout: 10000 }).should("be.visible")
     cy.getByData("my-build-tile").click()
     cy.url().should("include", "/my-builds")
     cy.getByData("my-build-title-showpage").should("have.text", "Interfacing RGB Led with Arduino")
   })
   it("The project should also be available from the homepage to non-logged in users", () => {
+    cy.visit("/edit-my-build/1")
+    cy.getByData("edit-project-title-input").should("have.value", "Interfacing RGB Led with Arduino")
+    cy.getByData("edit-project-form").submit()
     cy.logoutUser().should("have.property", "status", 200)
     cy.visit("/")
     cy.getByData("project-tile-homepage").click()
@@ -45,10 +39,10 @@ describe("When I submit a project", () => {
     cy.getByData("showpage-title").should("have.text", "Interfacing RGB Led with Arduino")
   })
   it("If the form does not contain the required information, the proper errors appear and the form is not submitted", () => {
+    cy.visit("/edit-my-build/1")
     cy.getByData("edit-project-title-input").clear()
     cy.getByData("edit-project-description-input").clear()
     cy.getByData("add-or-edit-instructions-button").click()
-    //tinymce-container must be clicked or the iframe body is not findable in the DOM
     cy.getByData("tinymce-container").should("exist").click()
     getIframeBody().should("contain", "RGB LED")
     getIframeBody().clear()
