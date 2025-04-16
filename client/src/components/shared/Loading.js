f4a2ac6054c07a590836206bc48f8291e09f0422
dleibe1 <daniel.leibensperger28@gmail.com>
2025-04-16 14:09:56 -0400

    changed loading animation

--- client/src/components/shared/Loading.js
+++ client/src/components/shared/Loading.js
@@ -4,7 +4,6 @@ import { Backdrop, CircularProgress, collapseClasses } from "@mui/material"
 const Loading = (props) => {
   return (
     <div style={{display: "flex", justifyContent: "center", alignItems: "center", height: "100vh"}}>
-      <h1 style={{ color: "#374146", paddingRight: "40px" }}>Loading...</h1>
       <CircularProgress size={80} style={{ color: "#374146" }} />
     </div>
   )
